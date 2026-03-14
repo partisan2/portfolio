28adf16210a4a69e75b8304903db4b9238637cee
Vihan Amarasingha <s92068813@ousl.lk>
2026-03-14 17:10:12 +0530

    Update hover effect color in Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
        <h3 className='text-3xl text-white px-10 py-10 text-center md:text-start'>EXPERIENCE.</h3>
        <div className='flex flex-wrap gap-12 mx-10 justify-start'>
           {experienceList?.map((el, index) => (
-            <div key={index} className='border-l-2 border-slate-700 pl-8 transition-all duration-500 hover:border-teal-500 max-w-[500px] py-2'>
+            <div key={index} className='border-l-2 border-slate-700 pl-8 transition-all duration-500 hover:border-[#38BDF8] max-w-[500px] py-2'>
               <div className='flex flex-col mb-4'>
                 <h3 className='text-xl text-white font-semibold transition-colors duration-300'>{el.jobTitle}</h3>
                 <h4 className='text-[#38BDF8] text-lg'>{el.company}</h4>
